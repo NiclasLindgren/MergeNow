baf5a55beef02c505b3b5a54d6caed0b8906db8f
kamil.wachala <kamil.wachala@gmail.com>
2024-10-17 18:06:07 +0200

    Create empty VSIX project

---
+++ src/MergeNow/MergeNowPackage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
+
+namespace MergeNow
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [ProvideAutoLoad(UIContextGuids80.NoSolution, PackageAutoLoadFlags.BackgroundLoad)]
+    [Guid(PackageGuidString)]
+    public sealed class PickAndMergePackage : AsyncPackage
+    {
+        public const string PackageGuidString = "0741861b-9c7e-4aad-a1d2-04379f0caa44";
+
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
